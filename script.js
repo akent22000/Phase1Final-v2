c2dc3218d29cde3d31afa38868e5b2e78507d2eb
akent22000 <62616574+akent22000@users.noreply.github.com>
2024-08-07 19:23:26 -0400

    Adjusted click cart and hover events

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 let quantity = 0;
 
-const divHeader = document.getElementById('header');
-const header = document.createElement('i');
-
-
-let i = document.createElement("i");
-i.classList.add("fa", "fa-shopping-cart")
-// i.textContent = "fa", "fa-trash-o"
-header.appendChild(i)
-divHeader.appendChild(header)
-
-
 //tie price to cart
-
-
-
 
 
 
@@ -30,12 +16,6 @@ function renderPrint(print) {
 
     const cardsContainer = document.getElementById('display');
 
-    const increaseBtn = document.createElement('button');
-    const decreaseBtn = document.createElement('button');
-    const countText = document.createElement('h3');
-
-    const cartBTN = document.createElement('button');
-    const cartText = document.createElement('h3');
 
     const myDetails = document.createElement('details');
     const description = document.createElement('summary');
@@ -45,20 +25,20 @@ function renderPrint(print) {
     const type = document.createElement('p');
     const price = document.createElement('p');
     const turnaround = document.createElement('p');
+    cartBTN = document.createElement('button');
+
+    const cartText = document.createElement('h3');
 
     div.setAttribute("class", "card");
     div.setAttribute("id", "myDetails");
 
-    increaseBtn.textContent = '+';
-    decreaseBtn.textContent = '-';
-
     description.textContent = 'Product Description'
     details.textContent = `${print.description}`
-    name.textContent = `Product: ${print.name}`
+    name.textContent = `${print.name}`
     type.textContent = `Category: ${print.type}`
     price.textContent = `Price: ${print.price}`
     turnaround.textContent = `Turn around time: ${print.turnaround}`
-    countText.textContent = quantity;
+
     cartText.textContent = 'Add to Cart';
     const image = document.createElement('img');
 
@@ -66,9 +46,17 @@ function renderPrint(print) {
     // like.classList = 'empty'
     image.src = print.image
     image.classList.add('image');
+    name.classList.add('name');
+
+
+    const text = document.createElement('h3');
+    text.classList.add('text');
+    text.textContent = 'Hover to see discount!';
+
+    div.appendChild(text)
+
 
     div.appendChild(image)
-
     div.appendChild(name)
     myDetails.appendChild(description)
     myDetails.appendChild(details)
@@ -76,31 +64,27 @@ function renderPrint(print) {
     div.appendChild(type)
     div.appendChild(price)
     div.appendChild(turnaround)
-    div.appendChild(increaseBtn)
-    div.appendChild(decreaseBtn)
-    div.appendChild(countText)
     div.appendChild(cartBTN)
     cartBTN.appendChild(cartText)
     cardsContainer.appendChild(div)
 
-
-    increaseBtn.addEventListener('click', (e) => {
+    cartBTN.addEventListener('click', (e) => {
         quantity++;
         countText.textContent = quantity;
-    });
-
-    decreaseBtn.addEventListener('click', (e) => {
-        if (quantity <= 0) {
-            quantity = 0;
-
-        } else {
-            quantity--;
-        }
-        countText.textContent = quantity;
 
     });
 
 
+    image.addEventListener('mouseover', () => {
+        text.textContent = '10% off';
+
+
+    });
+
+    image.addEventListener('mouseout', () => {
+        text.textContent = 'Hover to see discount!';
+
+    });
 
     document.getElementById("myDetails").addEventListener("toggle", (e) => {
 
@@ -108,6 +92,21 @@ function renderPrint(print) {
 
 }
 
+const divHeader = document.getElementById('header');
+const header = document.createElement('i');
+
+
+let i = document.createElement("i");
+i.classList.add("fa", "fa-shopping-cart")
+// i.textContent = "fa", "fa-trash-o"
+countText = document.createElement('h3');
+
+
+header.appendChild(countText)
+header.appendChild(i)
+divHeader.appendChild(header)
+
+// cartText.textContent = 'Add to Cart';
 
 // const divTest = document.getElementById('test');
 // const test = document.createElement('h3');
