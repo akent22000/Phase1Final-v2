9ce70e6cb1001c200c45b460f528e22f87f74600
akent22000 <62616574+akent22000@users.noreply.github.com>
2024-08-08 22:58:01 -0400

    fixed sort, added css attributes

--- script.js
+++ script.js
@@ -1,98 +1,146 @@
+
+
+
+//set shopping cart count varialbe
 let quantity = 0;
-let sort = 0
-console.log(sort)
+//set sort click event varialbe
+clicked = 0
 
-const url = 'http://localhost:3000/printing'
+//fetch json
+function fetchPrint() {
+    fetch('http://localhost:3000/printing')
+        .then((response) => response.json())
+        .then((print) => {
 
-fetch(url)
-    .then((response) => response.json())
-    .then((print) => {
-        render(print);
-    });
+            if (clicked === 1) {
+                document.getElementById("display").innerHTML = ''
+                print.sort((a, b) => a.price - b.price);
+            } else if (clicked === 0) {
+                print.sort((a, b) => {
+                    const nameA = a.name.toUpperCase();
+                    const nameB = b.name.toUpperCase();
+                    if (nameA < nameB) {
+                        return -1;
+                    }
+                    if (nameA > nameB) {
+                        return 1;
+                    }
+                    return 0;
+                });
+            }
+            print.forEach(print => {
+                renderPrint(print)
+            })
+        })
+}
+fetchPrint()
 
 
 sortBTN = document.createElement('button');
-sortBTN.addEventListener('click', (e) => {
-    print.sort((a, b) => a.price - b.price);
-
-
-});
-
+sortBtnText = document.createElement('h3');
 const divHeader = document.getElementById('header');
 const header = document.createElement('i');
-
-
-let i = document.createElement("i");
-i.classList.add("fa", "fa-shopping-cart")
 countText = document.createElement('h3');
+let i = document.createElement("i");
+
+i.classList.add("fa", "fa-shopping-cart")
+sortBTN.setAttribute("class", "btn btn-secondary");
+divHeader.setAttribute("class", "col-md-auto");
+sortBtnText.setAttribute("class", "cartText");
+
+countText.textContent = quantity;
+sortBtnText.textContent = "Sort by price"
 
 header.appendChild(countText)
 header.appendChild(i)
 divHeader.appendChild(header)
 divHeader.appendChild(sortBTN)
+sortBTN.appendChild(sortBtnText)
 
 
+sortBTN.addEventListener('click', (e) => {
+    clicked = 1
+    fetchPrint()
+});
 
-function render(print) {
-    //   print.sort((a, b) => a.price - b.price);
+function renderPrint(print) {
+    const cardsContainer = document.getElementById('display');
+    const myDetails = document.createElement('details');
+    const description = document.createElement('summary');
+    const details = document.createElement('p');
+    const div = document.createElement('div');
+    const name = document.createElement('h2');
+    const type = document.createElement('p');
+    const price = document.createElement('p');
+    const turnaround = document.createElement('p');
+    cartBTN = document.createElement('button');
+    const cartBtnText = document.createElement('h3');
 
-    print.forEach(myFunction);
-    function myFunction(print) {
-
-
-        const cardsContainer = document.getElementById('display');
-        const myDetails = document.createElement('details');
-        const description = document.createElement('summary');
-        const details = document.createElement('p');
-        const div = document.createElement('div');
-        const name = document.createElement('h2');
-        const type = document.createElement('p');
-        const price = document.createElement('p');
-        const turnaround = document.createElement('p');
-        cartBTN = document.createElement('button');
-
-        const cartText = document.createElement('h3');
-
-        div.setAttribute("class", "card");
-        div.setAttribute("id", "myDetails");
-
-        description.textContent = 'Product Description'
-        details.textContent = `${print.description}`
-        name.textContent = `${print.name}`
-        type.textContent = `Category: ${print.type}`
-        price.textContent = `Price: ${print.price}`
-        turnaround.textContent = `Turn around time: ${print.turnaround}`
-
-        cartText.textContent = 'Add to Cart';
-        const image = document.createElement('img');
-
-        image.classList = 'card-img'
-        // like.classList = 'empty'
-        image.src = print.image
-        image.classList.add('image');
-        name.classList.add('name');
+    div.setAttribute("class", "col-md-auto");
+    div.setAttribute("id", "myDetails");
+    cardsContainer.setAttribute("class", "row");
+    cartBTN.setAttribute("class", "btn btn-secondary");
+    cartBtnText.setAttribute("class", "cartText");
 
 
-        const text = document.createElement('h3');
-        text.classList.add('text');
+    description.textContent = 'Product Description'
+    details.textContent = `${print.description}`
+    name.textContent = `${print.name}`
+    type.textContent = `Category: ${print.type}`
+    price.textContent = `Price: ${print.price}`
+    turnaround.textContent = `Turn around time: ${print.turnaround}`
+
+    cartBtnText.textContent = 'Add to Cart';
+    const image = document.createElement('img');
+
+    image.classList = 'card-img'
+    image.src = print.image
+    image.classList.add('image');
+    name.classList.add('name');
+
+
+    const text = document.createElement('h3');
+    text.classList.add('text');
+    text.textContent = 'Hover to see discount!';
+
+    div.appendChild(text)
+
+
+    div.appendChild(image)
+    div.appendChild(name)
+    myDetails.appendChild(description)
+    myDetails.appendChild(details)
+    div.appendChild(myDetails)
+    div.appendChild(type)
+    div.appendChild(price)
+    div.appendChild(turnaround)
+    div.appendChild(cartBTN)
+    cartBTN.appendChild(cartBtnText)
+    cardsContainer.appendChild(div)
+
+    cartBTN.addEventListener('click', (e) => {
+        quantity++;
+        countText.textContent = quantity;
+
+    });
+
+    image.addEventListener('mouseover', () => {
+        if (print.price <= 50) {
+            text.textContent = '10% off';
+        } else if (print.price >= 51) {
+            text.textContent = '30% off';
+
+        }
+    });
+
+    image.addEventListener('mouseout', () => {
         text.textContent = 'Hover to see discount!';
 
-        div.appendChild(text)
+    });
 
+    document.getElementById("myDetails").addEventListener("toggle", (e) => {
 
-        div.appendChild(image)
-        div.appendChild(name)
-        myDetails.appendChild(description)
-        myDetails.appendChild(details)
-        div.appendChild(myDetails)
-        div.appendChild(type)
-        div.appendChild(price)
-        div.appendChild(turnaround)
-        div.appendChild(cartBTN)
-        cartBTN.appendChild(cartText)
-        cardsContainer.appendChild(div)
-    }
+    });
 }
-
 
 
